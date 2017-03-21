2f5d0f315084c36503b30b42190b1aae05c51e4f
Sam Makman <sammakman@gmail.com>
2017-03-21 13:38:20 -0700

    pray it works

--- lib/griddler/ses/railtie.rb
+++ lib/griddler/ses/railtie.rb
@@ -2,7 +2,7 @@ module Griddler
   module Ses
     class Railtie < Rails::Railtie
       initializer "griddler_ses.configure_rails_initialization" do |app|
-        Rails.application.middleware.insert_before  Griddler::Ses::Middleware
+        Rails.application.middleware.insert_before ActionDispatch::Executor, Griddler::Ses::Middleware
       end
     end
   end
